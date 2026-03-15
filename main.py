eda664749705b5db07bee71c6f74f30d64790b61
Victor Monteiro <133917272+SrVictor-Monteiro@users.noreply.github.com>
2026-03-15 16:41:32 -0300

    Update main.py test

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ while True:
     #Eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print('Quitting')
             pygame.quit()
             quit()
 
